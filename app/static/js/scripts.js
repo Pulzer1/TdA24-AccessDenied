716f8c0aa695249f3ee4d2fe465896bec2619d43
AlesGabrhelik <ales.gabrhelik@gmail.com>
2024-01-17 22:53:06 +0100

    lecturer frontend + Docker FIX

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -2,19 +2,26 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const savedTheme = localStorage.getItem('theme');
+    const LocIcon = document.getElementById("location_icon");
     if (savedTheme === 'dark') {
         document.documentElement.classList.add('dark');
         document.getElementById('darkmode-toggle').checked = true;
+        LocIcon.src = LocIcon.getAttribute("src-dark");
+    } else {
+        LocIcon.src = LocIcon.getAttribute("src-light");
     }
 });
 
 document.getElementById("darkmode-toggle").addEventListener('change', function() {
-    const LogoImage = document.getElementById("logo-image")
+    const LogoImage = document.getElementById("logo-image");
+    const LocIcon = document.getElementById("location_icon");
     if (this.checked) {
         document.documentElement.classList.add('dark');
         localStorage.setItem('theme', 'dark');
+        LocIcon.src = LocIcon.getAttribute("src-dark");
     } else {
         document.documentElement.classList.remove('dark');
         localStorage.setItem('theme', 'light');
+        LocIcon.src = LocIcon.getAttribute("src-light");
     }
 });
